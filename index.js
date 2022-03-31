fa140c629179f1c8134fbc49e7676ddac6d1fa00
juanjovita <juanjovita22@gmail.com>
2022-03-30 23:04:54 -0300

    CÓDIGO FINALIZADO

--- index.js
+++ index.js
@@ -22,5 +22,21 @@ while (adicinarMais !="não"){
   comida = prompt('Qual comida você deseja inserir?')
 
   categoria = prompt('Em qual categoria essa comida se encaixa: frutas, laticínios, doces ou congelados?')
+
+  if (categoria === 'frutas'){
+    frutas.push(comida)
+  }
+  else if(categoria === 'laticínios'){
+    laticinios.push(comida)
+  }
+  else if(categoria === 'doces'){
+    doces.push(comida)
+  }
+  else if(categoria === 'congelados'){
+    congelados.push(comida)
+  }
+
 }
+
+alert(`Lista de compras: \n Frutas: ${frutas} \n Laticínios: ${laticinios} \n Doces: ${doces} \n Congelados: ${congelados}`)
 
