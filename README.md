5fb4ae42efd9ddf7c845256d5eb1bf9047781a03
juanjovita <juanjovita22@gmail.com>
2022-03-30 21:45:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-tp8fse
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tp8fse)
---
+++ index.js
@@ -0,0 +1,21 @@
+// Import stylesheets
+import './style.css';
+let frutas = []
+let legumes = []
+let laticinios = []
+let comida = ""
+let categoria = "";
+
+let adicinarMais = "sim"
+
+while (adicinarMais !="não"){
+  adicinarMais = prompt('Você deseja adicionar uma comida na lista de compras? Responda SIM OU NÃO');
+  while (adicinarMais != "sim" && adicinarMais != "não"){
+    alert('você digitou um comando invalido')
+    adicinarMais = prompt('Você deseja adicionar uma comida na lista de compras? Responda SIM OU NÃO' )
+  }
+  if (adicinarMais === "não"){
+    break;
+  }
+}
+
